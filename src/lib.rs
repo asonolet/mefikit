59b178c7d0b1094c8887646987610e12ceecadb1
asonolet <aymeric.sonolet@gmail.com>
2025-05-03 18:05:40 +0200

    mv umesh in own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,9 @@
 mod mesh_element;
 mod element_block;
 mod element_block_like;
+mod umesh;
 
-pub use crate::mesh_element::ElementType;
-pub use crate::element_block::{ElementBlock, IntoElementBlockEntry, RegularCells, PolyCells};
-pub use crate::element_block_like::ElementBlockLike;
-
-pub mod umesh {
-
-    use ndarray::{Array1, Array2, ArrayD, ArrayView1, ArrayView2, ArrayViewD, Axis};
-    use std::collections::HashMap;
-    use std::collections::HashSet;
-    
-    pub use crate::mesh_element::ElementType;
-    pub use crate::element_block::{ElementBlock, IntoElementBlockEntry, RegularCells, PolyCells};
-    pub use crate::element_block_like::ElementBlockLike;
-
-    pub struct UMesh {
-        coords: Array2<f64>,
-        element_blocks: HashMap<ElementType, ElementBlock>,
-    }
-
-    pub struct UMeshView<'a> {
-        pub coords: &'a Array2<f64>,
-        pub elements: HashMap<ElementType, ElementBlockView<'a>>,
-    }
-    pub enum ElementBlockView<'a> {
-        RegularCells(&'a RegularCells),
-        PolyCells(&'a PolyCells),
-    }
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-    pub struct CellId {
-        pub element_type: ElementType,
-        pub local_index: usize,
-    }
-
-    impl UMesh {
-        pub fn add_compo<T: IntoElementBlockEntry>(&mut self, compo: T) {
-            let (key, wrapped) = compo.into_entry();
-            self.element_blocks.entry(key).or_insert(wrapped);
-        }
-    }
-}
+// pub use crate::mesh_element::ElementType;
+// pub use crate::element_block::{ElementBlock, IntoElementBlockEntry, RegularCells, PolyCells};
+// pub use crate::element_block_like::ElementBlockLike;
+pub use crate::umesh::UMesh;
---
+++ src/umesh.rs
@@ -0,0 +1,37 @@
+use ndarray::{Array1, Array2, ArrayD, ArrayView1, ArrayView2, ArrayViewD, Axis};
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+use crate::mesh_element::ElementType;
+use crate::element_block::{ElementBlock, IntoElementBlockEntry, RegularCells, PolyCells};
+use crate::element_block_like::ElementBlockLike;
+
+
+pub struct UMesh {
+    coords: Array2<f64>,
+    element_blocks: HashMap<ElementType, ElementBlock>,
+}
+
+pub struct UMeshView<'a> {
+    pub coords: &'a Array2<f64>,
+    pub elements: HashMap<ElementType, ElementBlockView<'a>>,
+}
+
+// TODO: write a real view (down to the ndarray view)
+pub enum ElementBlockView<'a> {
+    RegularCells(&'a RegularCells),
+    PolyCells(&'a PolyCells),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct CellId {
+    pub element_type: ElementType,
+    pub local_index: usize,
+}
+
+impl UMesh {
+    pub fn add_compo<T: IntoElementBlockEntry>(&mut self, compo: T) {
+        let (key, wrapped) = compo.into_entry();
+        self.element_blocks.entry(key).or_insert(wrapped);
+    }
+}
