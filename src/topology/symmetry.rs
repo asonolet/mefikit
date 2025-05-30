5e7106fd3f50401f198ca06afba666c730d9c0ae
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-05-30 23:36:27 +0200

    wip: start of symmetry implementation

---
+++ src/topology/symmetry.rs
@@ -0,0 +1,28 @@
+use crate::umesh::ElementType;
+
+pub trait ElementSymmetry {
+    const ROTATIONS: &'static [&'static [usize]];
+}
+
+
+// define_symmetries! {
+//     QUAD4 => {
+//         order: 4,
+//         rotations: [
+//             [0, 1, 2, 3],
+//             [1, 2, 3, 0],
+//             [2, 3, 0, 1],
+//             [3, 0, 1, 2],
+//         ]
+//     },
+//     HEX8 => {
+//         order: 8,
+//         rotations: [
+//             [0, 1, 2, 3, 4, 5, 6, 7],
+//             [1, 2, 3, 0, 5, 6, 7, 4],
+//             [2, 3, 0, 1, 6, 7, 4, 5],
+//             [3, 0, 1, 2, 7, 4, 5, 6],
+//         ]
+//     }
+// }
+
