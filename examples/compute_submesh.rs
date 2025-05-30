f150824e74059608e400b149a8f9360d8457a288
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-05-31 00:39:07 +0200

    refactor: minimal example about compute submesh

--- examples/compute_submesh.rs
+++ examples/compute_submesh.rs
@@ -4,10 +4,10 @@ use mefikit::compute_subentities;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Dummy mesh
 
-    let builder = RegularUMeshBuilder::new()
+    let mesh = RegularUMeshBuilder::new()
         .add_axis(vec![0.0, 1.0, 2.0])
-        .add_axis(vec![0.0, 10.0]);
-    let mesh = builder.build();
+        .add_axis(vec![0.0, 10.0])
+        .build();
     let (submesh, _, _) = compute_subentities(&mesh, None);
 
     println!("{mesh:?}");
