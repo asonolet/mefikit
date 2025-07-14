2caf92671d429f2ea0a3621c4514f0208057786a
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-07-14 14:34:23 +0200

    feat: add selection bench

---
+++ benches/umesh.rs
@@ -0,0 +1,44 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use mefikit::compute_submesh;
+use mefikit::RegularUMeshBuilder;
+
+fn submesh(c: &mut Criterion) {
+    let mut group = c.benchmark_group("submesh");
+
+    for i in [2, 10, 100] {
+        let mesh = RegularUMeshBuilder::new()
+            .add_axis((0..i).map(|i| i as f64).collect::<Vec<f64>>())
+            .add_axis((0..i).map(|i| i as f64).collect::<Vec<f64>>())
+            .build();
+        group.bench_with_input(BenchmarkId::new("mesh_size", i), &i, |b, _| {
+            b.iter(|| {
+                std::hint::black_box(compute_submesh(&mesh, None));
+            })
+        });
+    }
+}
+
+fn selection_sphere(c: &mut Criterion) {
+    let mut group = c.benchmark_group("selection");
+
+    for i in [2, 10, 40] {
+        let mesh = RegularUMeshBuilder::new()
+            .add_axis((0..i).map(|k| (k as f64) / (i as f64)).collect())
+            .add_axis((0..i).map(|k| (k as f64) / (i as f64)).collect())
+            .add_axis((0..i).map(|k| (k as f64) / (i as f64)).collect())
+            .build();
+        group.bench_with_input(BenchmarkId::new("mesh_size", i), &i, |b, _| {
+            b.iter(|| {
+                std::hint::black_box(
+                    mesh.select_ids()
+                        .centroids()
+                        .in_sphere(&[0.5, 0.5, 0.5], 0.25),
+                );
+            })
+        });
+    }
+}
+
+criterion_group!(bench, submesh, selection_sphere);
+criterion_main!(bench);
