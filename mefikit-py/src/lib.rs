b7a997fc44655eb503a6f36ffac303706162cd82
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-08-18 19:36:31 +0200

    feat(py): compile dummy mefikit-py

--- mefikit-py/src/lib.rs
+++ mefikit-py/src/lib.rs
@@ -1,22 +1,15 @@
 use pyo3::prelude::*;
 
-#[pyclass]
-struct UMesh {}
-
-#[pymethods]
-impl UMesh {
-    #[new]
-    fn new() -> Self {
-        UMesh {}
-    }
-
-    fn hello(&self) -> &'static str {
-        "Hello from Rust!"
-    }
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
 }
 
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
 #[pymodule]
-fn umesh_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<UMesh>()?;
-    Ok(())
+fn mefikit(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)
 }
