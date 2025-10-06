0542bf6c96dff50e9386174d40031827163c2d8c
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-10-06 10:58:35 +0200

    fix(examples): do not write submesh so it can be used as perf measurement

--- mefikit/examples/compute_submesh.rs
+++ mefikit/examples/compute_submesh.rs
@@ -1,5 +1,5 @@
 use mefikit as mf;
-use std::path::Path;
+use std::time;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Dummy mesh
@@ -11,8 +11,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .add_axis((0..=10).map(|i| i as f64 / 10.0).collect::<Vec<f64>>())
         .build();
     println!("Computing submesh");
-    let submesh = mf::topo::compute_submesh(&mesh, None, None);
+    let now = time::Instant::now();
+    for _ in 0..10 {
+        let _ = mf::topo::compute_submesh(&mesh, None, None);
+    }
+    let t_tot = now.elapsed().as_secs_f64() * 100.0;
 
-    println!("Writing submesh");
-    mf::write(&Path::new("examples/submesh.vtk"), submesh.view())
+    println!("Total elapsed time per op: {t_tot:}ms");
+
+    Ok(())
 }
