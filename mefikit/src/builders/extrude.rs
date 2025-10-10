50fb0d557d4ef82ae0c8ddc8421d54ebea6c6e7f
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2025-10-10 17:54:07 +0200

    WIP: extrude and duplicate api

--- mefikit/src/builders/extrude.rs
+++ mefikit/src/builders/extrude.rs
@@ -1,2 +1,14 @@
 // This module builds a mesh of one dimension higher than the input mesh by extuding it.
 // Duplicated nodes are allowed, both in the original mesh and the 1d mesh.
+
+use crate::{ElementType, UMesh, UMeshView};
+use ndarray::{ArcArray2, Array2};
+
+trait MeshTransform {
+    fn extrude_along(&self, path: &UMesh) -> UMesh;
+    fn duplicate_with(&self, vectors: &[[f64;3]]) -> UMesh;
+}
+
+impl MeshTransform for UMesh {
+    todo!();
+}
