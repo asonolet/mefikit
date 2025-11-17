48dbd973d0cdeed1af19650f8c12c631cf7c89dc
SONOLET Aymeric 259691 <as259691@is248687.intra.cea.fr>
2025-11-17 21:00:40 +0100

    wip: start symmetry class equality

--- mefikit/src/topology/symmetry.rs
+++ mefikit/src/topology/symmetry.rs
@@ -1,5 +1,39 @@
-pub trait ElementSymmetry {
-    const ROTATIONS: &'static [&'static [usize]];
+use crate::ElementLike;
+use rustc_hash::FxHashSet as HashSet;
+
+pub trait ElementEquality<'a>: ElementLike<'a> {
+    fn strict_equality(&self, other: &Self) -> bool {
+        if self.connectivity().len() != other.connectivity().len() {
+            return false;
+        }
+        self.connectivity()
+            .iter()
+            .zip(other.connectivity().iter())
+            .all(|(a, b)| a == b)
+    }
+    fn node_set_equality(&self, other: &Self) -> bool {
+        let self_nodes: HashSet<usize> = self.connectivity().iter().copied().collect();
+        let other_nodes: HashSet<usize> = other.connectivity().iter().copied().collect();
+        self_nodes == other_nodes
+    }
+    // fn strict_equivalence(&self, other: &Self) -> bool {
+    //     if self.connectivity().len() != other.connectivity().len() {
+    //         return false;
+    //     }
+    //     for symmetry in symmetries {
+    //         let mut is_equivalent = true;
+    //         for (i, &node_idx) in symmetry.iter().enumerate() {
+    //             if self.connectivity()[i] != other.connectivity()[node_idx] {
+    //                 is_equivalent = false;
+    //                 break;
+    //             }
+    //         }
+    //         if is_equivalent {
+    //             return true;
+    //         }
+    //     }
+    //     false
+    // }
 }
 
 // define_symmetries! {
