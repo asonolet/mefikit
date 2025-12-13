76f43fb46c348e84815604af7ce8be523264ad6e
SONOLET Aymeric 259691 <as259691@is248687.intra.cea.fr>
2025-12-13 18:18:42 +0100

    tool(ruff): disable rules on __init__.py

--- mefipy/python/mefikit/__init__.py
+++ mefipy/python/mefikit/__init__.py
@@ -1,13 +1,16 @@
+# ruff: disable[F403,F405,F821,E402]
 from .mefikit import *
-from . import io
-from . import data
 
 __doc__ = mefikit.__doc__
 if hasattr(mefikit, "__all__"):
     __all__ = mefikit.__all__
 else:
     __all__ = ()
+del mefikit
+
+from . import io as io
+from . import data
+# ruff: enable[F403,F405,F821,E402]
+
 
 __all__ = (*__all__, "data")
-
-del mefikit
