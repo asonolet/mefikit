a64b96822ec7fb99141ef029533b037e46c0378d
SONOLET Aymeric 259691 <as259691@is248687.intra.cea.fr>
2025-12-13 16:39:54 +0100

    feat: add data module to have some mesh examples

--- mefikit-py/python/mefikit/__init__.py
+++ mefikit-py/python/mefikit/__init__.py
@@ -1,10 +1,13 @@
 from .mefikit import *
 from . import io
+from . import data
 
 __doc__ = mefikit.__doc__
 if hasattr(mefikit, "__all__"):
     __all__ = mefikit.__all__
 else:
     __all__ = ()
+
+__all__ = (*__all__, "data")
 
 del mefikit
---
+++ mefikit-py/python/mefikit/data.py
@@ -0,0 +1,32 @@
+import mefikit as mf
+import numpy as np
+
+
+def umesh3():
+    umesh = mf.UMesh(np.arange(30, dtype=float).reshape((10, 3)))
+    umesh.add_regular_block(
+        "TRI3", np.array([[0, 1, 2], [1, 2, 3], [2, 3, 4]], dtype=np.uint)
+    )
+    umesh.add_regular_block(
+        "QUAD4", np.array([[0, 1, 2, 3], [4, 5, 6, 7]], dtype=np.uint)
+    )
+    umesh.add_regular_block("HEX8", np.array([[0, 1, 2, 3, 4, 5, 6, 7]], dtype=np.uint))
+    return umesh
+
+
+def cmesh3():
+    umesh = mf.build_cmesh(
+        np.linspace(0.0, 0.5, 10), np.linspace(0.0, 1.0, 20), range(3)
+    )
+    smesh2 = umesh.submesh()
+    for et, block in smesh2.blocks().items():
+        umesh.add_regular_block(et, block)
+    smesh1 = smesh2.submesh()
+    for et, block in smesh1.blocks().items():
+        umesh.add_regular_block(et, block)
+    return umesh
+
+
+def umesh2():
+    umesh = mf.build_cmesh(range(5), np.linspace(0.0, 1.0))
+    return umesh
