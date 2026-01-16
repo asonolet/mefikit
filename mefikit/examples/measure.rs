2967efd2be0f46aad3f4a8fe14d945b65f02ec1a
SONOLET Aymeric 259691 <as259691@is248687.intra.cea.fr>
2026-01-16 15:48:51 +0100

    bench: increase size of measure example

--- mefikit/examples/measure.rs
+++ mefikit/examples/measure.rs
@@ -5,17 +5,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Dummy mesh
 
     let mesh = mf::RegularUMeshBuilder::new()
-        .add_axis((0..=100).map(|i| i as f64 / 10.0).collect::<Vec<f64>>())
-        .add_axis((0..=100).map(|i| i as f64 / 10.0).collect::<Vec<f64>>())
+        .add_axis((0..=1000).map(|i| i as f64 / 10.0).collect::<Vec<f64>>())
+        .add_axis((0..=1000).map(|i| i as f64 / 10.0).collect::<Vec<f64>>())
         .build();
     let mut t_tot = 0.0;
-    for _ in 0..10000 {
+    let n = 100;
+    for _ in 0..n {
         let now = time::Instant::now();
         let _ = mf::measure(mesh.view());
         let elapsed = now.elapsed();
         t_tot += elapsed.as_secs_f64();
     }
-    t_tot /= 10.0;
+    t_tot *= 1000.0 / (n as f64);
     println!("Total elapsed time per op: {t_tot:}ms");
     Ok(())
 }
