bd78197ad96e35fb6701aca3035538b52edba3f6
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2026-02-23 16:26:11 +0100

    feat: convert mf::field to dict

--- mefipy/src/pyfield.rs
+++ mefipy/src/pyfield.rs
@@ -1,3 +1,6 @@
+use std::collections::BTreeMap;
+
+use ndarray as nd;
 use pyo3::prelude::*;
 
 use mefikit::prelude as mf;
@@ -6,4 +9,18 @@ use mefikit::prelude as mf;
 #[pyo3(name = "Field")]
 pub struct PyField {
     inner: mf::FieldOwnedD,
+}
+
+impl From<BTreeMap<mf::ElementType, nd::ArrayD<f64>>> for PyField {
+    fn from(value: BTreeMap<mf::ElementType, nd::ArrayD<f64>>) -> Self {
+        PyField {
+            inner: mf::FieldOwnedD::new(value),
+        }
+    }
+}
+
+impl From<PyField> for BTreeMap<mf::ElementType, nd::ArrayD<f64>> {
+    fn from(value: PyField) -> Self {
+        value.inner.0
+    }
 }
