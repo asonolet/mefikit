72de12097b41e54dcdfc909f2cb6077a82531c94
asonolet <aymeric.sonolet@cea.fr>
2026-04-20 16:36:42 +0200

    fix(py): conditionnaly monkey patch

--- src/mefikit/__init__.py
+++ src/mefikit/__init__.py
@@ -1,8 +1,16 @@
+import importlib.util
+
 from . import data as data
 from . import io
 from .mefipy import UMesh, build_cmesh, sel
 
-io.install_conversions()
+
+def has(name: str) -> bool:
+    return importlib.util.find_spec(name) is not None
+
+
+if has("meshio") and has("medcoupling") and has("pyvista"):
+    io.install_conversions()
 del io
 
 __all__ = ("UMesh", "build_cmesh", "data", "sel")
