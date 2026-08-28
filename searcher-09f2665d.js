07f61398060d3ffaa6fc20a0cff0133f5b66a1f5
asonolet <asonolet@users.noreply.github.com>
2026-08-28 12:16:45 +0000

    deploy: 50ba39f5376240efd881b4437cb90d2b17067bf2

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d39a58f9.js',
+                path_to_root + 'searchindex-5cba43aa.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
